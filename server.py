862de68dd99379a1d043b623c845015a71803637
Denis Matakov <denis@matakov.com>
2021-07-30 14:17:29 +0300

    Async streaming of async zipped bytes stream. Wow!

--- server.py
+++ server.py
@@ -2,46 +2,71 @@ import asyncio
 
 from aiohttp import web
 import aiofiles
-import datetime
 
 INTERVAL_SECS = 1
+CHUNK_SIZE = 1000 * 8  # 100 KB
+IMAGES_PATH = "test_photos/"
 
 
-async def uptime_handler(request):
-    response = web.StreamResponse()
+async def archive(folder):
+    """Archive the folder asynchronously."""
+    # flag "-" helps to redirect output bytes to stdout
+    # flag "-j" do not include parent directory in archive
+    command = f"zip -r -j - {folder}"
+    archive_process = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,  # catch archive bytes from stdout
+        stderr=asyncio.subprocess.PIPE,
+    )
 
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
-    response.headers['Content-Type'] = 'text/html'
-
-    # Отправляет клиенту HTTP заголовки
-    await response.prepare(request)
-
+    byte_archive = bytes()
     while True:
-        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+        # read process output part by part
+        part = await archive_process.stdout.read(n=CHUNK_SIZE)
+        if not part:
+            break
+        byte_archive += part
+        yield part
 
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
+    # for testing purposes we can save file.
+    # with open(folder + ".zip", "wb") as f:
+    #     f.write(byte_archive)
 
 
 async def archivate(request):
-    await uptime_handler(request)
+    archive_hash = request.match_info.get("archive_hash", "photos")
+    folder = IMAGES_PATH + archive_hash
+    response = web.StreamResponse()
+
+    # File download header
+    response.headers[
+        "Content-Disposition"
+    ] = f'attachment; filename="{archive_hash}.zip"'
+
+    # Send headers =>
+    await response.prepare(request)
+    # Now we must not change headers
+
+    # Start async streaming bytes
+    async for part in archive(folder):
+        await response.write(part)
+
+    # Notify end side that we are done streaming!
+    await response.write_eof()
 
 
 async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
+    async with aiofiles.open("index.html", mode="r") as index_file:
         index_contents = await index_file.read()
-    return web.Response(text=index_contents, content_type='text/html')
+    return web.Response(text=index_contents, content_type="text/html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = web.Application()
-    app.add_routes([
-        web.get('/', handle_index_page),
-        web.get('/timer/', uptime_handler),
-        web.get('/archive/{archive_hash}/', archivate),
-    ])
+    app.add_routes(
+        [
+            web.get("/", handle_index_page),
+            web.get("/archive/{archive_hash}/", archivate),
+        ]
+    )
     web.run_app(app)
